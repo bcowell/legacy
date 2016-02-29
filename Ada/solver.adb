b34e5724a408f274794bc6a146a9ecf230ed1fa8
Brayden Cowell <bcowell@mail.uoguelph.ca>
2016-02-29 02:48:40 -0500

    Progressing

--- Ada/solver.adb
+++ Ada/solver.adb
@@ -9,31 +9,68 @@ procedure Solver is
 
 	type array_type is array (1..9) of Integer;
 	type puzzle_type is array (1..9, 1..9) of Integer;
-	type grid_type is array (1..3, 1..3) of array_type; -- Create a 3x3 grid able to store an array of 9 possibilites
+	type grid_array_type is array (1..3, 1..3) of array_type; -- Create a 3x3 grid able to store an array of 9 possibilites
+	type grid_type is array (1..3, 1..3) of Integer;
 
 	knowns : array_type := (1..9 => 0);
 	values : array_type := (1..9 => 0);
-	tempArray : array_type := (1..9 => 0);
+	possibilites : array_type := (1..9 => 0);
+	temp_array : array_type := (1..9 => 0);
 	
-	grid : grid_type;
-	puzzle : puzzle_Type;
+	grid : grid_array_type := (1..3 => (1..3 => (1..9 => 0)));
+	temp_grid : grid_type := (1..3 => (1..3 => 0));
+	puzzle : puzzle_Type := (
+	(0,0,0, 2,6,0, 7,0,1),
+	(6,8,0, 0,7,0, 0,9,0),
+	(1,9,0, 0,0,4, 5,0,0),
+	(8,2,0, 1,0,0, 0,4,0),
+	(0,0,4, 6,0,2, 9,0,0),
+	(0,5,0, 0,0,3, 0,2,8),
+	(0,0,9, 3,0,0, 0,7,4),
+	(0,4,0, 0,5,0, 0,3,6),
+	(7,0,3, 0,1,8, 0,0,0));
+	
 	known_value : Boolean;
 	puzzle_conflict : Boolean;
+	check_grid : Boolean;
 	
-	x,y,n : Integer; -- Keep track of what 3x3 grid we are on of the whole puzzle.
+	temp_value : Integer;
+	x,y,n,m,t : Integer;
+
+
+	-- Solved_check
+	-- Go through a 3x3 grid and see if its "solved" (i.e. One value filled in each square).
+	-- Returns TRUE on solved, FALSE otherwise
+	function grid_checked (grid : in grid_array_type; puzzle : in puzzle_type; posX : in Integer; posY : in Integer) return Boolean is
+		n : Integer;
+	begin
+		for i in 1..3 loop
+			for j in 1..3 loop
+				-- Make sure there's only one element in the array of possibilites of each point
+				n := 0;
+				for k in 1..9 loop
+					if (values(k) /= 0) then
+						n := n + 1;
+					end if;
+				end loop;
+				if (n > 1) then
+					return FALSE;
+				end if;
+			end loop;
+		end loop;
+		return TRUE;
+	end grid_checked;
+	
 	
 	-- Check 
     -- See if value has a conflict with rest of the numbers in its column and row
     -- Check if number is in same col or row of puzzle
     -- Return TRUE on conflict, FALSE otherwise
-    function check(num : in Integer; puzzle : in puzzle_type; posX : in Integer; posY : in Integer) return Boolean is
+    function check (num : in Integer; puzzle : in puzzle_type; posX : in Integer; posY : in Integer) return Boolean is
     begin
 		-- Go through rows of puzzle sharing PosY
 		for i in 1..9 loop
 			if (num = puzzle(i, posY)) then
-				put(num);
-				put(puzzle(i,posY));
-				new_line;
 				return TRUE;
 			end if;
 		end loop;
@@ -41,19 +78,16 @@ procedure Solver is
 		-- Go through cols of puzzle sharing PosX
 		for j in 1..9 loop
 			if (num = puzzle(posX, j)) then
-				put(num);
-				put(puzzle(posY, j));
-				new_line;
 				return TRUE;
 			end if;
 		end loop;
 		
         return FALSE;
     end check;
+   
     
-    
-	-- Store possibilities for all squares with no values in the 3x3 grid
-	-- Possible values include those not in the current grid
+	-- Get all the possibilities for each square in the 3x3 grid
+	-- Possible values include those not in the current grid (Can't be in known)
 	function possible (knowns : in array_type) return array_type is
 	begin
 		for k in 1..9 loop -- Go through all the possible numbers
@@ -72,33 +106,15 @@ procedure Solver is
 		end loop;
 		return values;
 	end possible;
+
 	
 	begin
-	
-	-- Initialize 2D array with 81 empty grids (nx = 9, ny = 9)
-	for i in 1..9 loop
-		for j in 1..9 loop
-			puzzle (i, j) := 0;
-		end loop;
-	end loop;
 	
 	-- Read from file and fill in know values.
 	-- filename: string(1..50);
 	-- last: natural;
 	-- get_line(filename,last);
 	--open(infp,in_file,fileN(1..last));
-	
-	-- Just assign one for now..
-	puzzle := (
-	(0,0,0, 2,6,0, 7,0,1),
-	(6,8,0, 0,7,0, 0,9,0),
-	(1,9,0, 0,0,4, 5,0,0),
-	(8,2,0, 1,0,0, 0,4,0),
-	(0,0,4, 6,0,2, 9,0,0),
-	(0,5,0, 0,0,3, 0,2,8),
-	(0,0,9, 3,0,0, 0,7,4),
-	(0,4,0, 0,5,0, 0,3,6),
-	(7,0,3, 0,1,8, 0,0,0));
 	
 	
 	-- Print it out the unsolved puzzle.
@@ -114,79 +130,135 @@ procedure Solver is
 	y := 0;
 	n := 1;
 	
-	-- Get the values already in the 3x3 grid
-	for i in 1..3 loop
-		for j in 1..3 loop
-			case puzzle(x+i, y+j) is 
-				when 0 => null;
-				when others => knowns(n) := puzzle(x+i, y+j); -- Copy the know values into an array for later
-			end case;
-			n := n+1;
+	-- Search
+	-- DFS look through all possible values of each square in the grid and try them all out
+	loop -- while (grid has > 1 value)
+			-- Calculate all possible values of each square
+	
+		-- Get the known values already in the 3x3 grid
+		for i in 1..3 loop
+			for j in 1..3 loop
+				case puzzle(x+i, y+j) is 
+					when 0 => null;
+					when others => knowns(n) := puzzle(x+i, y+j); -- Copy the know values into an array for later
+				end case;
+				n := n+1;
+			end loop;
 		end loop;
-	end loop;
-	
-	values := possible(knowns);
-	
-	put_line("Possible numbers in grid:");
-	for i in 1..9 loop
-		if (values(i) /= 0) then
-			put(values(i));
-		end if;
-	end loop;
-	new_line;
-	new_line;
-	
-	for i in 1..3 loop
-		for j in 1..3 loop
-			if (puzzle(x+i, y+j) = 0) then
-				-- Narrow down possibilities even more by checking if there is an already filled number that conflicts with any of the possibilites
-				for k in 1..9 loop
-					puzzle_conflict := FALSE;
-					if (values(k) /= 0) then
-						puzzle_conflict := check(values(k), puzzle, x+i, y+j);
-						-- Returns FALSE on conflicts
-						if (puzzle_conflict = TRUE) then
-							values(k) := 0;
+		
+		-- Calculate all the possibilites of each point of the 3x3 grid
+		-- Not taking into account any conflicts in same rows or cols yet.
+		values := possible(knowns);
+		
+		-- Narrow down possibilities even more by checking if there are any conflicts in cols / rows of the entire puzzle
+		for i in 1..3 loop
+			for j in 1..3 loop
+				if (puzzle(x+i, y+j) = 0) then
+					for k in 1..9 loop
+						puzzle_conflict := FALSE;
+						if (values(k) /= 0) then
+							puzzle_conflict := check(values(k), puzzle, x+i, y+j);
+							-- Returns FALSE on conflicts
+							if (puzzle_conflict = TRUE) then
+								values(k) := 0;
+							end if;
 						end if;
+					end loop;
+					grid(i,j) := values;
+				else -- Already filled in puzzle, put in first position of the array at the point
+					temp_array(1) := puzzle(x+i, y+j);
+					grid(i,j) := temp_array;
+					temp_grid(i,j) := temp_array(1);
+				end if;
+				values := possible(knowns);
+			end loop;
+		end loop;
+		
+		n := 0;
+		m := 1;
+		
+		-- If there is only one possible number, fill it in.
+		for i in 1..3 loop
+			for j in 1..3 loop
+				temp_array := grid(i,j);
+				for k in 1..9 loop
+					if (temp_array(k) /= 0) then
+						n := n + 1;
 					end if;
 				end loop;
-				put_line("Next value(k)");
-				grid(i,j) := values;
-			else -- Already
-				tempArray(1) := puzzle(x+i, y+j);
-				grid(i,j) := tempArray;
-			end if;
-			values := possible(knowns);
-		end loop;
-	end loop;
-	
-	-- Now we have a 3x3 array with each point being an array of possible solutions
-	-- Points with one solution are an array of 1..9 with the first filled and the rest 0.
-	new_line;
-	new_line;
-	for i in 1..3 loop
-		for j in 1..3 loop
-			tempArray := grid(i,j);
-			for k in 1..9 loop
-				if (tempArray(k) /= 0) then
-					put(tempArray(k));
+				-- If a possible number is the only value and is in a possible value move it to the front
+				if (n = 1) then
+					for h in 2..9 loop
+						if (temp_array(h) /= 0) then
+							temp_array(1) := temp_array(h);
+							temp_array(h) := 0;
+							grid(x+i, y+j) := temp_array;
+							temp_grid(x+i, y+j) := temp_array(1);
+						end if;
+					end loop;
 				end if;
+				-- Save the number of possibilites of each point, so we can pick the minimum later.
+				possibilites(m) := n;
+				n := 0;
+				m := m + 1;
+			end loop;
+		end loop;
+		
+		-- Now we have a 3x3 array with each point being an array of possible solutions
+		-- Points with one solution are an array of 1..9 with the first filled and the rest 0.
+
+		-- Choose an empty square with the least possibilities
+		-- Go through the possibilites and find the smallest
+		m := 9;
+		t := 0;
+		for k in 1..9 loop
+			if ((possibilites(k) /= 1) and (m > possibilites(k))) then
+				m := possibilites(k);
+			end if;
+		end loop;
+		-- Find the first instance of the smallest possibility.
+		for k in 1..9 loop
+			if (possibilites(k) = m) then
+				t := k;
+			end if;
+		end loop;
+		
+		-- pick a value for the current square
+		m := 1;
+		for i in 1..3 loop
+			for j in 1..3 loop
+				if (t = m) then -- Found the minimum to start testing.
+					-- Go through its possible values and pick one
+					temp_array := grid(x+i, y+j);
+					for k in 1..9 loop
+						if (temp_array(k) /= 0) then
+							temp_value := temp_array(k); -- Grab a possible value
+							temp_grid(x+i, y+j) := temp_value; -- Add it into place in a temp_grid
+						end if;
+					end loop;
+				end if;
+				m := m + 1;
+			end loop;
+		end loop;
+		
+		
+		for i in 1..3 loop
+			for j in 1..3 loop
+				put(temp_grid(x+i, y+j));
 			end loop;
 			new_line;
 		end loop;
+		new_line;
+		new_line;
+		
+		-- check if the chosen value is fine
+		
+			-- if false; remove failed value from spot
+			-- if true; update grid with newfound value
+		check_grid := grid_checked(grid, puzzle, x, y);
+		exit when check_grid = TRUE;
 	end loop;
-	
-	
-	-- Search
-	-- DFS look through all possible values of each square in the grid and try them all out
-		-- while (grid has > 1 value)
-			-- Calculate all possible values of each square
-			-- Choose an empty square with the least possibilities
-			-- pick a value
-			-- check if the chosen value is fine
-				-- if false; remove failed value from spot
-				-- if true; update grid with newfound value
-			-- Recursively call search
+   
    
 	-- Print out solved puzzle
 	-- for i in 1..9 loop
